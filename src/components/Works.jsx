ba1c9657f5eef17c85c9eb49aa204c4eb8baa3f7
shagar49 <shagar49@outlook.com>
2023-08-21 07:23:19 +0530

    final output

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -36,7 +36,17 @@ const ProjectCard = ({
           <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
             <div
               onClick={() => window.open(source_code_link, "_blank")}
-              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+              className="green-pink-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+            >
+              <img
+                src={github}
+                alt="source code"
+                className="w-1/2 h-1/2 object-contain"
+              />
+            </div>
+            <div
+              onClick={() => window.open(source_code_link, "_blank")}
+              className="black-gradient ml-2 w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
             >
               <img
                 src={github}
